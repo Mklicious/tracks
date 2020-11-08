be43feb6d4c4acbd87c64a9708d808a475470bd5
Mickael <mickael.doranges@gmail.com>
2020-11-08 11:45:52 +0100

    safe area views

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
 const TrackCreateScreen = () => {
     return (
-        <>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text style={{ fontSize: 48 }}>track create screen</Text>
-        </>
+        </SafeAreaView>
     );
 };
 
