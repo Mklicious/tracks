36298376fc7db2d81a40124aaeac7578d6fbf782
Mickael <mickael.doranges@gmail.com>
2020-11-21 07:50:03 +0100

    automatic auth

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,24 @@
 import axios from 'axios';
+import  AsyncStorage  from '@react-native-community/async-storage';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'http://adba926bc5b7.ngrok.io'
 });
+
+//le token sera automatiquement renseigné à chaque requete.
+instance.interceptors.request.use(
+    //fonction executée à chaque requete
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    //fonction executée si erreur
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
